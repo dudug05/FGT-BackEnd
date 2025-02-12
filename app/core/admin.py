e36c86ba68da6e046a5bc8e269ec25d55f51cc8d
dudugazuforget <dudu99744@gmail.com>
2025-02-12 20:09:52 -0300

    creating the first tables

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,60 @@
-from django.contrib import admin
+"""
+Django admin customization.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal Info"), {"fields": ("name", "passage_id")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login",)}),
+        (_("Groups"), {"fields": ("groups",)}),
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(models.Usuario)
+admin.site.register(models.Empresa)
+admin.site.register(models.Cliente)
+admin.site.register(models.Financa)
+admin.site.register(models.Servico)
+admin.site.register(models.Produto)
+admin.site.register(models.Pedido)
+admin.site.register(models.ItemPedido)
