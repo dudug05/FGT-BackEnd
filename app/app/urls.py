b3287878a04b1a0d5a6e85f6a62088a86dea57cf
dudugazuforget <dudu99744@gmail.com>
2025-02-12 20:09:52 -0300

    creating the first tables

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,22 +1,28 @@
-"""
-URL configuration for app project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter 
+
+from core.views import UsuarioViewSet  
+from core.views import EmpresaViewSet
+from core.views import ClienteViewSet
+from core.views import FinancaViewSet
+from core.views import ServicoViewSet
+from core.views import ProdutoViewSet
+from core.views import PedidoViewSet
+from core.views import ItemPedidoViewSet
+
+
+router = DefaultRouter()
+router.register(r"usuario", UsuarioViewSet)
+router.register(r"empresa", EmpresaViewSet)
+router.register(r"cliente", ClienteViewSet)
+router.register(r"financa", FinancaViewSet)
+router.register(r"servico", ServicoViewSet)
+router.register(r"produto", ProdutoViewSet)
+router.register(r"pedido", PedidoViewSet)
+router.register(r"itempedido", ItemPedidoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),  
 ]
